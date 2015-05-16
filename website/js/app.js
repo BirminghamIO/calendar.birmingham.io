10a4ec51965068e354301a2b944cacd99917ccae
Stuart Langridge <sil@kryogenix.org>
2015-05-16 16:45:40 +0100

    Show map inline when the location is tapped

--- website/js/app.js
+++ website/js/app.js
@@ -16,4 +16,19 @@ document.addEventListener("DOMContentLoaded", function() {
     updateToGo();
 });
 
+document.addEventListener("click", function(e) {
+    if (e.target.nodeName.toLowerCase() == "a" && e.target.className == "loc") {
+        e.preventDefault();
+        var ifrs = e.target.parentNode.getElementsByTagName("iframe");
+        if (ifrs.length > 0) {
+            ifrs[0].parentNode.removeChild(ifrs[0]);
+            return;
+        }
+        var ifr = document.createElement("iframe");
+        ifr.className = "map";
+        ifr.src = e.target.href.replace("google.com/maps?q", "google.com/maps/embed/v1/place?key=AIzaSyDGmKkBfxNrIqWZ3WNpZznrdMqHD9yV2fM&q");
+        e.target.parentNode.insertBefore(ifr, e.target);
+    }
+});
+
 setInterval(updateToGo, 60000);
